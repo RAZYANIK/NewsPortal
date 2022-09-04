20e028e215a72d1ddb03323c7c2e4ae93a1fff0f
RAZYANIK <mdfarhanrazy@gmail.com>
2022-09-04 11:23:00 +0600

    4 commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,19 @@ const newsCategories = async () => {
     newsCategoriesQantainer(data.data.news_category)
         .catch((error) => console.log(error));
 };
+const newsCategoriesQantainer = (types) => {
+    console.log(types);
+    const categoriesContainer = document.getElementById("newsCatagory");
+
+    types.forEach((category) => {
+
+        const newsItem = document.createElement("p");
+        newsItem.innerHTML = `
+        <p style="cursor: context-menu" class="text-white text-center fw-semibold " href="" onclick="loadNews(${category.category_id})">
+            ${category.category_name}
+        </p>
+        `;
+        categoriesContainer.appendChild(newsItem);
+    });
+
+};
